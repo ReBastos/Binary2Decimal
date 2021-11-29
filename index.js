a5c1d82feaa9c09173abdc0af4f68d1e7c1a065b
Renato <renato.bastos96@gmail.com>
2021-11-29 18:18:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.getElementById("submit").addEventListener("click", set);
 
 function set() {
     
-    let inputValue = `${document.getElementById("binary").value}`;
+    let inputValue = document.getElementById("binary").value;
     
     document.getElementById('value').textContent = binaryToDecimal(inputValue);
     
