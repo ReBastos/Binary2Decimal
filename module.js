c19043b609cbce902b601411c4e139f93d892bc5
Renato <renato.bastos96@gmail.com>
2021-12-01 18:56:10 -0300

    Update module.js

--- module.js
+++ module.js
@@ -1,23 +1,40 @@
 
 const binaryToDecimal = (binary) => {
 
-    let strLenght = binary.length;
-    strLenght -= 1;
+    let binaryLenght = binary.length;
+    let arrayBinary = binary.split('');
+    binaryLenght -= 1;
     let total = 0;
+    let testBinary = 0;
 
+    arrayBinary.forEach((valor) =>{
+        if(valor !== '1' && valor !== '0'){
+            testBinary++;
+            console.log(testBinary);
+        }
+
+    });
+
+    if( testBinary == 0){
 
     for(var i = 0; i < binary.length; i++){
         
 
-        total = total +  ((Math.pow(2, i)) * parseInt(binary[strLenght]));
+        total = total +  ((Math.pow(2, i)) * parseInt(binary[binaryLenght]));
 
         
-        strLenght--;
+        binaryLenght--;
 
 
     }
 
     return total;
+
+    } else {
+        window.alert("Digite um valor Binario!");
+    }
+
+
 
 }
 
